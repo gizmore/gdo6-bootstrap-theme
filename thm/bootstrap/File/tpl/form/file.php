fa5e9cf10b91322bc3bfd12becbff9bbcd748963
gizmore <gizmore@wechall.net>
2019-12-28 00:15:47 +0100

    6.10-r1044_php7_4

--- thm/bootstrap/File/tpl/form/file.php
+++ thm/bootstrap/File/tpl/form/file.php
@@ -1,18 +1,5 @@
 <?php /** @var $field \GDO\File\GDT_File **/ ?>
-<div class="gdo-file-controls">
-<div id="gdo-file-preview-<?=$field->name?>"></div>
-<?php foreach ($field->getInitialFiles() as $file) : $file instanceof \GDO\File\GDO_File; ?>
-<?php $deleteButton = sprintf('<input type="submit" name="delete_%s[%s]" value="Remove File" onclick="return confirm(\'%s\')"/>', $field->name, $file->getID(), t('confirm_delete')); ?>
-<?php if ($field->preview && $file->isImageType()) : ?>
-<?php printf('<div class="gdo-file-preview"><img src="%s" />%s (%s)</div>', $field->displayPreviewHref($file), $deleteButton, html($file->getName())); ?>
-<?php else : ?>
-<?php printf('<div class="gdo-file-preview">%s %s</div>', html($file->getName()), $deleteButton); ?>
-<?php endif; ?>
-<?php endforeach; ?>
-  <div style="clear: both;"></div>
-</div>
-
-<div class="form-group <?=$field->classError()?>">
+<div class="form-group gdo-flow-file <?=$field->classError()?>">
   <?=$field->htmlTooltip()?>
   <?=$field->htmlIcon()?>
   <?=$field->displayLabel()?>
@@ -21,7 +8,6 @@
         <span class="btn btn-primary">
             Browse&hellip;
             <input
-             id="my-file-selector"
 <?php if ($field->multiple) : ?>
              multiple="multiple"
 <?php endif; ?>
@@ -30,7 +16,11 @@
              style="display:none;" />
         </span>
     </label>
-    <input type="text" class="form-control" id="upload-file-info" readonly>
+    <input
+     type="text"
+     class="form-control"
+     id="gdo-file-preview-<?=$field->name?>"
+     readonly>
   </div>
   <?= $field->htmlError(); ?>
 </div>
