bdfd02c63d46b1c5420ea0698abd3909e6bbc202
gizmore <gizmore@wechall.net>
2019-12-28 04:23:07 +0100

    6.10-r1045_fixes

--- thm/bootstrap/Form/tpl/form/submit.php
+++ thm/bootstrap/Form/tpl/form/submit.php
@@ -1,7 +1,12 @@
-<?php use GDO\Form\GDT_Submit; $field instanceof GDT_Submit; ?>
+<?php
+use GDO\Form\GDT_Submit;
+/** @var $field GDT_Submit **/
+$field instanceof GDT_Submit;
+?>
 <input
  type="submit"
  class="btn btn-primary"
- name="<?= $field->name; ?>"
- value="<?= $field->displayLabel(); ?>"
- <?= $field->htmlDisabled(); ?> /></input>
+ name="<?=$field->name?>"
+ value="<?=$field->displayLabel()?>"
+ <?=$field->htmlAttributes()?>
+ <?=$field->htmlDisabled()?> /></input>
--- thm/bootstrap/UI/tpl/cell/iconbutton.php
+++ thm/bootstrap/UI/tpl/cell/iconbutton.php
@@ -2,10 +2,10 @@
 /** @var $field \GDO\UI\GDT_IconButton **/
 ?>
 <?php if ($href) : ?>
-<button
+<div
  <?=$field->htmlDisabled()?>
  class="btn <?=$field->primary?'btn-primary':'btn-secondary'?> gdt-button">
   <a
    href="<?=$href?>"><?=$field->htmlIcon()?> <?=$field->displayLabel()?></a>
-</button>
+</div>
 <?php endif; ?>
