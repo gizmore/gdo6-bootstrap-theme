1e943f585598ae44f4ee50982d72ead40baaeddd
gizmore <gizmore@wechall.net>
2019-12-28 01:49:34 +0100

    6.10-r1044_fixes

--- thm/bootstrap/UI/tpl/cell/panel.php
+++ thm/bootstrap/UI/tpl/cell/panel.php
@@ -1,5 +1,12 @@
-<?php /** @var $field \GDO\UI\GDT_Panel; **/ ?>
-<div class="card">
+<?php
+use GDO\UI\GDT_Panel;
+/**
+ * @var $field GDT_Panel;
+**/
+$field instanceof GDT_Panel;
+$field->addClass('card');
+?>
+<div <?=$field->htmlAttributes()?>">
   <div class="card-body">
     <p class="card-text"><?=$field->html?></p>
 <?php foreach($field->fields as $field) : ?>
