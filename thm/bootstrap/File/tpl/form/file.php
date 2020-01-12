73e57c3aee9517627fdd8c4e187d1bc9da56b5b5
gizmore <gizmore@wechall.net>
2020-01-12 12:31:20 +0100

    6.10-r4242_payment_stuff

--- thm/bootstrap/File/tpl/form/file.php
+++ thm/bootstrap/File/tpl/form/file.php
@@ -1,4 +1,22 @@
-<?php /** @var $field \GDO\File\GDT_File **/ ?>
+<?php
+use GDO\File\GDO_File;
+use GDO\UI\GDT_Icon;
+/** @var $field \GDO\File\GDT_File **/
+$file = $field->getValue();
+$file instanceof GDO_File;
+?>
+
+<div class="form-group">
+  <img 
+   id="preview-image-<?=$field->name?>"
+   class="gdt-file-image-preview"
+<?php if ($file && $file->isPersisted()) : ?>
+   src="<?=$field->displayPreviewHref($file)?>" />
+<?php else : ?>
+<?php endif; ?>
+</div>
+
+
 <div class="form-group gdo-flow-file <?=$field->classError()?>">
   <?=$field->htmlTooltip()?>
   <?=$field->htmlIcon()?>
@@ -20,7 +38,18 @@
      type="text"
      class="form-control"
      id="gdo-file-preview-<?=$field->name?>"
-     readonly>
+<?php if ($file && $file->isPersisted()) : ?>
+     value="<?=$file->displayName()?>"
+<?php endif; ?>
+     readonly />
+<?php if ($file && $file->isPersisted()) : ?>
+<?php 
+$icon = GDT_Icon::make()->icon('delete')->render();
+$deleteButton = sprintf('<button class="gdt-file-remove" type="submit" name="delete_%s[%s]" value="Remove File">%s</button>', $field->name, $file->getID(), $icon);
+echo $deleteButton;
+?>
+<?php endif; ?>
+    
   </div>
   <?= $field->htmlError(); ?>
 </div>
