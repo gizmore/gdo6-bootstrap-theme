a75606eac48a462e3dee65d80376cf4c6f6b1855
gizmore <gizmore@wechall.net>
2020-02-07 01:48:58 +0100

    6.10-r5005_sync

---
+++ thm/bootstrap/Date/tpl/form/duration.php
@@ -0,0 +1,13 @@
+<?php /** @var $field \GDO\DB\GDT_Duration **/ ?>
+<div class="form-group <?=$field->classError()?>">
+  <?=$field->htmlIcon()?>
+  <label for="form[<?= $field->name; ?>]"><?= $field->displayLabel(); ?></label>
+  <input
+   class="form-control"
+   type="text"
+   name="form[<?= $field->name; ?>]"
+   <?= $field->htmlDisabled(); ?>
+   <?= $field->htmlRequired(); ?>
+   value="<?= $field->displayVar(); ?>" />
+  <?=$field->htmlError()?>
+</div>
