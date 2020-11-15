505921b06cd16743d3702699a4b8609f3bb985b8
gizmore <gizmore@wechall.net>
2020-11-15 19:54:13 +0100

    6.10-r9379_primarybutton

--- thm/bootstrap/UI/tpl/cell/iconbutton.php
+++ thm/bootstrap/UI/tpl/cell/iconbutton.php
@@ -4,7 +4,7 @@
 <?php if ($field->href) : ?>
 <div
  <?=$field->htmlDisabled()?>
- class="btn <?=$field->primary?'btn-primary':'btn-secondary'?> gdt-button gdt-image-button">
+ class="btn <?=$field->primaryButton?'btn-primary':'btn-secondary'?> gdt-button gdt-image-button">
   <a
    href="<?=$field->href?>"><?=$field->htmlIcon()?> <?=$field->displayLabel()?></a>
 </div>
