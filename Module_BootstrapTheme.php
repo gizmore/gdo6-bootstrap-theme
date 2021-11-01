7e2ca9c04597dfdbe5a16e8d7cc2c8c5f696f1d9
gizmore <gizmore@wechall.net>
2021-11-01 09:56:56 +0100

    6.10.6-r6162_hooks

--- Module_BootstrapTheme.php
+++ Module_BootstrapTheme.php
@@ -34,25 +34,25 @@ final class Module_BootstrapTheme extends GDO_Module
     		$min = Module_Javascript::instance()->jsMinAppend();
     		
     		$this->addBowerCSS("bootstrap-slider/dist/css/bootstrap-slider$min.css");
-    		$this->addBowerJavascript("bootstrap-slider/dist/bootstrap-slider$min.js");
+    		$this->addBowerJS("bootstrap-slider/dist/bootstrap-slider$min.js");
     		
     		$this->addBowerCSS("pc-bootstrap4-datetimepicker/build/css/bootstrap-datetimepicker$min.css");
-    		$this->addBowerJavascript("pc-bootstrap4-datetimepicker/build/js/bootstrap-datetimepicker.min.js");
+    		$this->addBowerJS("pc-bootstrap4-datetimepicker/build/js/bootstrap-datetimepicker.min.js");
     		
-    		$this->addBowerJavascript("autocomplete.js/dist/autocomplete.jquery$min.js");
+    		$this->addBowerJS("autocomplete.js/dist/autocomplete.jquery$min.js");
     		
     		$this->addBowerCSS("bootstrap-select/dist/css/bootstrap-select$min.css");
-    		$this->addBowerJavascript("bootstrap-select/dist/js/bootstrap-select$min.js");
+    		$this->addBowerJS("bootstrap-select/dist/js/bootstrap-select$min.js");
     		
     		$this->addCSS("css/gdo-bootstrap-theme-sidebar2.css");
     		$this->addCSS("css/gdo-bootstrap-theme-autocomplete.css");
     		$this->addCSS("css/gdo-bootstrap-theme-gdo.css");
     		$this->addCSS("css/gdo-bootstrap-theme-vote.css");
     
-    		$this->addJavascript("js/gdo-bootstrap-theme.js");
+    		$this->addJS("js/gdo-bootstrap-theme.js");
     	
     		$this->addCSS("css/tagsinput.css");
-    		$this->addJavascript("js/tagsinput.js");
+    		$this->addJS("js/tagsinput.js");
 	    }
 	}
 	
