bef38af85ae7e59525ffa285df94fe617254ad1b
gizmore <gizmore@wechall.net>
2021-11-27 22:41:24 +0100

    6.11.0-r6202

--- thm/bootstrap/UI/tpl/cell/tab.php
+++ thm/bootstrap/UI/tpl/cell/tab.php
@@ -6,9 +6,9 @@ $field instanceof GDT_Tab;
   <h5><?=$field->displayLabel()?></h5>
   <div>
 <?php
-foreach ($field->getFields() as $gdoType)
+foreach ($field->getFields() as $gdt)
 {
-	echo $cell ? $gdoType->renderCell() : $gdoType->renderForm();
+	echo $cell ? $gdt->renderCell() : $gdt->renderForm();
 }
 ?>
   </div>
