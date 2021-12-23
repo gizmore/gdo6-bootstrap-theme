19b2f53c9591bfe1204835241fc21c657055cf27
gizmore <gizmore@wechall.net>
2021-12-23 06:06:32 +0100

    6.11.1-r6268

--- js/gdo-bootstrap-theme.js
+++ js/gdo-bootstrap-theme.js
@@ -1,5 +1,30 @@
 "use strict"
 $(function () {
+	
+	window.GDO.error = function(html, title) {
+		let ok = t('btn_ok');
+		title = title || t('sitename');
+		let modal = `
+<div class="modal" tabindex="-1" role="dialog">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">${title}</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p>${html}</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary" data-dismiss="modal">${ok}</button>
+      </div>
+    </div>
+  </div>
+</div>`;
+		return $(modal).modal();
+	};
 
 	window.GDO.Language = {
 		'switch': function(select) {
